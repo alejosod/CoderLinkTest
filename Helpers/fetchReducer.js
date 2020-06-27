165885ed02a1912eb5b22f69de9c949768b59fc3
alex <a.soler@blackstone.studio>
2020-06-27 12:10:03 -0500

    fetch reducer done

---
+++ Helpers/fetchReducer.js
@@ -0,0 +1,38 @@
+const SET_LOADING = 'SET_LOADING';
+const SET_ERROR = 'SET_ERROR';
+const SET_SUCCESS = 'SET_DATA';
+
+export const setLoading = () => ({ type: SET_LOADING });
+export const setError = (error) => ({ type: SET_ERROR, payload: error });
+export const setSuccess = (data) => ({ type: SET_SUCCESS, payload: data });
+
+export default (state, action) => {
+    const { type } = action;
+
+    switch (type) {
+        case SET_LOADING:
+            return {
+                loading: true,
+                error: false,
+                success: false
+            }
+        case SET_ERROR: {
+            const { payload } = action;
+            return {
+                loading: false,
+                error: payload,
+                success: false,
+            }
+        }
+        case SET_SUCCESS: {
+            const { payload } = action;
+            return {
+                loading: false,
+                error: false,
+                success: payload,
+            }
+        }
+        default:
+            throw new Error('Error at fetch Reducer: No Action type specified');
+    }
+}
